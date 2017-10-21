a7b392b7c6532af6706db2bd6e1528101deeafb3
jdamador <amadorsalasjd@gmail.com>
2017-10-21 05:15:21 -0600

    Create the package an any class

---
+++ MazeRunner/src/pk/codeapp/model/Frame.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pk.codeapp.model;
+
+/**
+ *This class is the  in charge of make vértices del grafo
+ * @author Daniel Amador
+ */
+public class Frame
+{
+    
+}
