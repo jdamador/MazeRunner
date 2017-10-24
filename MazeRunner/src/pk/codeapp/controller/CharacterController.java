4402171ef4f7e833ccc89992ffdb4f76a076887e
Jose Pablo Brenes Alfaro <josepablo1997@hotmail.com>
2017-10-23 20:52:17 -0600

    short jumps and roadshortJumps

--- MazeRunner/src/pk/codeapp/controller/CharacterController.java
+++ MazeRunner/src/pk/codeapp/controller/CharacterController.java
@@ -5,7 +5,10 @@
  */
 package pk.codeapp.controller;
 
+import java.util.ArrayList;
 import java.util.Random;
+import pk.codeapp.model.Frame;
+import pk.codeapp.model.Link;
 
 /**
  * This method is charge of all character's functions
@@ -15,20 +18,131 @@ import java.util.Random;
 public class CharacterController {
 
     private int numberCharacters;
-
+    ArrayList<Frame> listRoute = new ArrayList();
+    ArrayList<Frame> listRouteShort = new ArrayList();
+        ArrayList<Frame> listRoadRoute = new ArrayList();
+    ArrayList<Frame> listRoadRouteShort = new ArrayList();
+    private Frame pointerCharacter1;
+    private int counterWeight = 0;
+    private int Weight = 0;
     /**
      * This method calc a random number
      *
      * @param limit
      * @return
      */
+    
+    private Frame GlobalOriginP1;
+
     public int getRandom(int minimum, int maximum) {
         int randomInt = minimum + (int) (Math.random() * maximum);
         return randomInt;
     }
-    
+
     public void numbreCharacters() {
-        numberCharacters=getRandom(2, 3);
+        numberCharacters = getRandom(2, 3);
+    }
+
+    public void shortRouteJumps(Frame origin, Frame destination,Frame GlobalOrigin) {
+        if (origin == null) {
+            System.out.println("Nulo");
+            return;
+        }
+        if (origin.isMark() == true) {
+            System.out.println("marca");
+            if (origin == GlobalOrigin) {
+                clean_Mark();
+            }
+        }
+        origin.setMark(true);
+        Link aux = origin.getNextLink();
+        System.out.println("Agrego: " + origin.getName());
+        listRoute.add(origin);
+        while (aux != null) {
+            if (aux.getDestiny().equals(destination)) {
+                if (listRouteShort.size() == 0) {
+                    listRoute.add(aux.getDestiny());
+                    for (int i = 0; i < listRoute.size(); i++) {
+                        listRouteShort.add(listRoute.get(i));
+                    }
+                    return;
+                }
+                if (listRoute.size() < listRouteShort.size()) {
+                     listRoute.add(aux.getDestiny());
+                    for (int i = 0; i < listRoute.size(); i++) {
+                        listRouteShort.add(listRoute.get(i));
+                    }
+                    return;
+                }
+            }
+            shortRouteJumps(aux.getDestiny(), destination,GlobalOrigin);
+            aux = aux.getNextLink();
+
+        }
+    }
+    public void roadShortJumps(Frame origin, Frame destination,Frame globalOrigin)
+        {
+             if (origin == null){
+                 System.out.println("Nulo");
+                return;}
+             if (origin.isMark() == true){
+                 System.out.println("marca");
+              if(origin==globalOrigin){
+                    clean_Mark();
+              }
+                    } 
+                origin.setMark(true);
+                Link aux = origin.getNextLink();
+                System.out.println("Agrego: "+origin.getName());
+                listRoadRouteShort.add(origin);
+                while (aux != null)
+                {
+                   if(aux.getDestiny().equals(destination)){
+                       listRoadRouteShort.add(destination);
+                         if(listRoadRoute.size()==0){ 
+                               listRoadRoute=listRoadRouteShort;
+                               listRoadRouteShort.add(globalOrigin);
+                           return;
+                       }
+                       if(listRoadRouteShort.size()> listRoadRoute.size()){
+                           System.out.println("Listas Iguales");
+                           listRoadRoute=listRoadRouteShort;
+                           }
+                      listRoadRouteShort.add(globalOrigin);
+                   }
+                   roadShortJumps(aux.getDestiny(), destination, globalOrigin);
+                    aux = aux.getNextLink();
+            }
+    }
+    public void clean_Mark() {
+        Frame temp = pointerCharacter1;
+        if (pointerCharacter1 == null) {
+            return;
+        }
+        while (temp != null) {
+            temp.setMark(false);
+            temp = temp.getNextFrame();
+        }
+    }
+ public void addCharacterFrame(String name, boolean token, int row, int column, boolean allow) {
+        Frame square = new Frame(name, token, row, column);
+        square.setAllow(allow);
+        square.setNextFrame(null);
+        if (pointerCharacter1 == null) {
+            pointerCharacter1 = square;
+        } else {
+            square.setNextFrame(pointerCharacter1);
+            pointerCharacter1 = square;
+        }
+    }
+ 
+    
+    public Frame getGlobalOriginP1() {
+        return GlobalOriginP1;
+    }
+
+    public void setGlobalOriginP1(Frame GlobalOrigin) {
+        this.GlobalOriginP1 = GlobalOrigin;
     }
 
 }
--- MazeRunner/src/pk/codeapp/view/GameWindows.java
+++ MazeRunner/src/pk/codeapp/view/GameWindows.java
@@ -32,27 +32,12 @@ public class GameWindows extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        jLabel2 = new javax.swing.JLabel();
-        jLabel1 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jPanel1.setOpaque(false);
         jPanel1.setPreferredSize(new java.awt.Dimension(800, 800));
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-
-        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/pk/codeapp/view/tools/Character2.gif"))); // NOI18N
-        jLabel2.setBorder(new javax.swing.border.MatteBorder(null));
-        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(800, 10, -1, -1));
-
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/pk/codeapp/view/tools/character3.gif"))); // NOI18N
-        jLabel1.setBorder(new javax.swing.border.MatteBorder(null));
-        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(800, 210, 70, 70));
-
-        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/pk/codeapp/view/tools/character1.gif"))); // NOI18N
-        jLabel3.setBorder(new javax.swing.border.MatteBorder(null));
-        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(800, 100, 70, 70));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -106,9 +91,6 @@ public class GameWindows extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
