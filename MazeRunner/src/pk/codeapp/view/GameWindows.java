78cb0c4ff9d2ea4f4df2fe53d2f56a07fbd028e9
jdamador <amadorsalasjd@gmail.com>
2017-10-28 01:22:29 -0600

    solve fails in the form

--- MazeRunner/src/pk/codeapp/view/GameWindows.java
+++ MazeRunner/src/pk/codeapp/view/GameWindows.java
@@ -29,44 +29,32 @@ public class GameWindows extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-<<<<<<< HEAD
         imageCharacter2 = new javax.swing.JLabel();
         imageCharacter3 = new javax.swing.JLabel();
         imageCharacter1 = new javax.swing.JLabel();
-=======
->>>>>>> developer
-
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
         jPanel1.setOpaque(false);
         jPanel1.setPreferredSize(new java.awt.Dimension(800, 800));
-<<<<<<< HEAD
         jPanel1.setLayout(null);
-
         imageCharacter2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         imageCharacter2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/pk/codeapp/view/tools/Character2.gif"))); // NOI18N
         imageCharacter2.setBorder(new javax.swing.border.MatteBorder(null));
         imageCharacter2.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
         jPanel1.add(imageCharacter2);
         imageCharacter2.setBounds(540, 100, 72, 72);
-
         imageCharacter3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         imageCharacter3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/pk/codeapp/view/tools/character3.gif"))); // NOI18N
         imageCharacter3.setBorder(new javax.swing.border.MatteBorder(null));
         imageCharacter3.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
         jPanel1.add(imageCharacter3);
         imageCharacter3.setBounds(70, 120, 80, 80);
-
         imageCharacter1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         imageCharacter1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/pk/codeapp/view/tools/character1.gif"))); // NOI18N
         imageCharacter1.setBorder(new javax.swing.border.MatteBorder(null));
         imageCharacter1.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
         jPanel1.add(imageCharacter1);
         imageCharacter1.setBounds(340, 270, 80, 80);
-=======
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
->>>>>>> developer
-
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -135,12 +123,9 @@ public class GameWindows extends javax.swing.JFrame {
     }    
     
     // Variables declaration - do not modify//GEN-BEGIN:variables
-<<<<<<< HEAD
     public static javax.swing.JLabel imageCharacter1;
     public static javax.swing.JLabel imageCharacter2;
     public static javax.swing.JLabel imageCharacter3;
-=======
->>>>>>> developer
     private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
